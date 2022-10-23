397dab191f8067bc729ea0faf9f9d4687ab94242
Vaitomarnocu6927 <116380632+Vaitomarnocu6927@users.noreply.github.com>
2022-10-23 13:04:11 -0400

    Add files via upload

---
+++ assets/preload/data/expurgation/SongName__Port.lua
@@ -0,0 +1,5 @@
+function onCreate()
+makeLuaText('watermark', songName .. " OPT BY VICTORPRO", 0, -2, 700); -- You can edit the created by Prisma Text just dont change anything else
+    setTextSize('watermark', 20);
+    addLuaText('watermark');
+end
